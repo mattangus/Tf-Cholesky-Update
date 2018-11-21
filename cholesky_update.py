b926b5a8a76b8de12fd082daec1d59938519d621
Matt Angus <m2angus@gsd.uwaterloo.ca>
2018-11-21 08:32:23 -0500

    added path to library

--- cholesky_update.py
+++ cholesky_update.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
+import os
 
+base = os.path.dirname(__file__)
+
+#keep the ./ in case base is empty
 try:
-    _lib = tf.load_op_library('./build/libcholesky_update.so')
+    _lib = tf.load_op_library(os.path.join(base, "./build/libcholesky_update.so"))
 except Exception as e:
-    _lib = tf.load_op_library('./libcholesky_update.so')
+    _lib = tf.load_op_library(os.path.join(base, "./libcholesky_update.so"))
 chol_update = _lib.chol_update
