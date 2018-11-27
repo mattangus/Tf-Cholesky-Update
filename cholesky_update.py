04b4e3cb6cc37ddccc2e3a2fa192462aaf831201
Matt Angus <m2angus@gsd.uwaterloo.ca>
2018-11-27 02:18:11 -0500

    changed to metric variable

--- cholesky_update.py
+++ cholesky_update.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import os
+from tensorflow.python.ops import array_ops
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import variable_scope
 
 base = os.path.dirname(__file__)
 
@@ -10,7 +13,20 @@ except Exception as e:
     _lib = tf.load_op_library(os.path.join(base, "./libcholesky_update.so"))
 _chol_update = _lib.chol_update
 
-def cholesky_update(x, mask, init=1e-5):
+#taken from metrics_impl. Need custom init function
+def _metric_variable(shape, dtype, initializer=lambda: array_ops.zeros(shape, dtype), validate_shape=True, name=None):
+    # Note that synchronization "ON_READ" implies trainable=False.
+    return variable_scope.variable(
+        initializer,
+        collections=[
+            ops.GraphKeys.LOCAL_VARIABLES, ops.GraphKeys.METRIC_VARIABLES
+        ],
+        validate_shape=validate_shape,
+        synchronization=variable_scope.VariableSynchronization.ON_READ,
+        aggregation=variable_scope.VariableAggregation.SUM,
+        name=name)
+
+def cholesky_update(x, mask, init=1e-5, trainable=False):
     """Create a variable `L` that is the cholesky decomposition of a matrix `A = LL^T`
 
     if `x` has dimensions `[b,dim]` then mask must have dimensions `[b]` and `L` will
@@ -35,9 +51,10 @@ def cholesky_update(x, mask, init=1e-5):
 
     assert len(input_shape) == 2, "rank of x must be 2"
 
-    L_init = tf.eye(input_shape[1],batch_shape=[input_shape[0]])*init
+    L_init = lambda: tf.eye(input_shape[1],batch_shape=[input_shape[0]])*init
 
-    L = tf.get_variable("L", initializer=L_init)
+    L = _metric_variable(input_shape + [input_shape[1]], tf.float32, initializer=L_init, name="L")
+    #L = tf.get_variable("L", initializer=L_init, trainable=trainable)
 
     update_op = _chol_update(L, x, mask)
 
